e742419f8b0e98efd82a731366e1267b67d5ba89
Marcio Curvello <marciocurvello@gmail.com>
2023-08-16 23:34:01 -0300

    add scrollview

--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -1,34 +1,32 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, ScrollView } from "react-native";
 import React from "react";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 
 export default function WelcomeScreen() {
   return (
-    <View style={styles.container}>
+    <ScrollView indicatorStyle="white" style={styles.container}>
       <Text style={styles.title}>Welcome to Little Lemon</Text>
       <Text style={styles.description}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear more about your experience with us!
       </Text>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.9,
+    flex: 1,
   },
   title: {
-    fontSize: 30,
+    fontSize: 50,
     fontWeight: "600",
     padding: 40,
     color: "#edefee",
     textAlign: "center",
   },
   description: {
-    fontSize: 24,
+    fontSize: 38,
     fontWeight: "400",
     padding: 30,
     color: "#edefee",
