d01b0e2cbad25000f03f344a86124e068a6436e2
Marcio Curvello <marciocurvello@gmail.com>
2023-08-18 23:48:44 -0300

    feat: add menu items

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import { StyleSheet, View } from "react-native";
 import Header from "./src/components/Header";
 import Footer from "./src/components/Footer";
 import WelcomeScreen from "./src/screens/WelcomeScreen";
+import MenuItems from "./src/components/MenuItems";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-      <WelcomeScreen />
+      <MenuItems />
       <Footer />
     </View>
   );
