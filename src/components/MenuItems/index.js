c684675ce19b63efee548a3b3dfe3ee4c86426a7
Marcio Curvello <marciocurvello@gmail.com>
2023-08-22 22:30:12 -0300

    update to SectionList

--- src/components/MenuItems/index.js
+++ src/components/MenuItems/index.js
@@ -1,28 +1,48 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet, SectionList } from "react-native";
 import React from "react";
 
 const menuItemsToDisplay = [
-  { name: "Hummus", price: "$5.00", id: "1A" },
-  { name: "Moutabal", price: "$5.00", id: "2B" },
-  { name: "Falafel", price: "$7.50", id: "3C" },
-  { name: "Marinated Olives", price: "$5.00", id: "4D" },
-  { name: "Kofta", price: "$5.00", id: "5E" },
-  { name: "Eggplant Salad", price: "$8.50", id: "6F" },
-  { name: "Lentil Burger", price: "$10.00", id: "7G" },
-  { name: "Smoked Salmon", price: "$14.00", id: "8H" },
-  { name: "Kofta Burger", price: "$11.00", id: "9I" },
-  { name: "Turkish Kebab", price: "$15.50", id: "10J" },
-  { name: "Fries", price: "$3.00", id: "11K" },
-  { name: "Buttered Rice", price: "$3.00", id: "12L" },
-  { name: "Bread Sticks", price: "$3.00", id: "13M" },
-  { name: "Pita Pocket", price: "$3.00", id: "14N" },
-  { name: "Lentil Soup", price: "$3.75", id: "15O" },
-  { name: "Greek Salad", price: "$6.00", id: "16Q" },
-  { name: "Rice Pilaf", price: "$4.00", id: "17R" },
-  { name: "Baklava", price: "$3.00", id: "18S" },
-  { name: "Tartufo", price: "$3.00", id: "19T" },
-  { name: "Tiramisu", price: "$5.00", id: "20U" },
-  { name: "Panna Cotta", price: "$5.00", id: "21V" },
+  {
+    title: "Appetizers",
+    data: [
+      { name: "Hummus", price: "$5.00" },
+      { name: "Moutabal", price: "$5.00" },
+      { name: "Falafel", price: "$7.50" },
+      { name: "Marinated Olives", price: "$5.00" },
+      { name: "Kofta", price: "$5.00" },
+      { name: "Eggplant Salad", price: "$8.50" },
+    ],
+  },
+  {
+    title: "Main Dishes",
+    data: [
+      { name: "Lentil Burger", price: "$10.00" },
+      { name: "Smoked Salmon", price: "$14.00" },
+      { name: "Kofta Burger", price: "$11.00" },
+      { name: "Turkish Kebab", price: "$15.50" },
+    ],
+  },
+  {
+    title: "Sides",
+    data: [
+      { name: "Fries", price: "$3.00", id: "11K" },
+      { name: "Buttered Rice", price: "$3.00" },
+      { name: "Bread Sticks", price: "$3.00" },
+      { name: "Pita Pocket", price: "$3.00" },
+      { name: "Lentil Soup", price: "$3.75" },
+      { name: "Greek Salad", price: "$6.00" },
+      { name: "Rice Pilaf", price: "$4.00" },
+    ],
+  },
+  {
+    title: "Desserts",
+    data: [
+      { name: "Baklava", price: "$3.00" },
+      { name: "Tartufo", price: "$3.00" },
+      { name: "Tiramisu", price: "$5.00" },
+      { name: "Panna Cotta", price: "$5.00" },
+    ],
+  },
 ];
 
 const Item = ({ name, price }) => (
@@ -35,12 +55,20 @@ const Item = ({ name, price }) => (
 export default function MenuItems() {
   const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
+  const renderSectionHeader = ({ section: { title } }) => (
+    <View style={styles.headerStyle}>
+      <Text style={styles.sectionHeader}>{title}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <FlatList
+      <SectionList
+        sections={menuItemsToDisplay}
         data={menuItemsToDisplay}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) => item + index}
         renderItem={renderItem}
+        renderSectionHeader={renderSectionHeader}
       />
     </View>
   );
@@ -59,5 +87,14 @@ const styles = StyleSheet.create({
   itemText: {
     color: "#F4CE14",
     fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: "#F4CE14",
+  },
+  sectionHeader: {
+    fontSize: 26,
+    textAlign: "center",
+    color: "#000",
+    flexWrap: "wrap",
   },
 });
