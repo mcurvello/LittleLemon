e263c6407651fbc5ca6e3a1a290fec8ec8abb0ba
Marcio Curvello <marciocurvello@gmail.com>
2023-09-18 01:41:43 -0300

    add logo

--- src/screens/WelcomeScreen/index.js
+++ src/screens/WelcomeScreen/index.js
@@ -1,4 +1,11 @@
-import { Text, StyleSheet, ScrollView, TextInput } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
 import React, { useState } from "react";
 
 export default function WelcomeScreen() {
@@ -6,6 +13,16 @@ export default function WelcomeScreen() {
 
   return (
     <ScrollView indicatorStyle="white" style={styles.container}>
+      <View style={styles.headerWrapper}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/img/logo.png")}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel="Little Lemon Logo"
+        />
+        <Text style={styles.headerText}>Little Lemon</Text>
+      </View>
       <Text style={styles.title}>Welcome to Little Lemon</Text>
       <Text style={styles.description}>
         Little Lemon is a charming neighborhood bistro that serves simple food
@@ -25,6 +42,25 @@ export default function WelcomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  headerText: {
+    paddingTop: 30,
+    paddingLeft: 20,
+    paddingRight: 10,
+    paddingBottom: 10,
+    fontSize: 30,
+    color: "#edefee",
+    textAlign: "center",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 20,
   },
   title: {
     fontSize: 40,
