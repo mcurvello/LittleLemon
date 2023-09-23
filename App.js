57d17a18b7ad1091af6f49e4225d851c7c8f4e87
Marcio Curvello <marciocurvello@gmail.com>
2023-09-23 10:51:02 -0300

    add tab navigation

--- App.js
+++ App.js
@@ -7,27 +7,38 @@ import LoginScreen from "./src/screens/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    // <NavigationContainer>
-    //   <View style={styles.container}>
-    //     <Header />
-    //     <Stack.Navigator initialRouteName="Login">
-    //       <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    //       <Stack.Screen name="Login" component={LoginScreen} />
-    //     </Stack.Navigator>
-    //     <Footer />
-    //   </View>
-    // </NavigationContainer>
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Welcome" component={WelcomeScreen} />
-        <Tab.Screen name="Login" component={LoginScreen} />
-      </Tab.Navigator>
+      <View style={styles.container}>
+        <Header />
+        {/* <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator> */}
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ size }) => {
+              let iconName;
+              if (route.name === "Welcome") {
+                iconName = "ios-home";
+              } else if (route.name === "Login") {
+                iconName = "ios-enter";
+              }
+              return <Ionicons name={iconName} size={size} />;
+            },
+          })}
+        >
+          <Tab.Screen name="Welcome" component={WelcomeScreen} />
+          <Tab.Screen name="Login" component={LoginScreen} />
+        </Tab.Navigator>
+        <Footer />
+      </View>
     </NavigationContainer>
   );
 }
